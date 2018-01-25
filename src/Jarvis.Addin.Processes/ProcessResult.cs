439d3b310e43e888e04cad34a4263489bf736987
Unknown <jonstodle@outlook.com>
2018-01-26 00:06:45 +0100

    Add ProcessResult

---
+++ src/Jarvis.Addin.Processes/ProcessResult.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using Jarvis.Core;
+using JetBrains.Annotations;
+
+namespace Jarvis.Addin.Processes
+{
+    [UsedImplicitly]
+    [DebuggerDisplay("{" + nameof(Title) + ",nq}")]
+    internal struct ProcessResult : IQueryResult, IEquatable<ProcessResult>
+    {
+        public int ProcessId { get; }
+        public string Title { get; }
+        public string Description { get; }
+        public float Distance { get; }
+        public float Score { get; }
+        public QueryResultType Type => QueryResultType.Application;
+
+        public ProcessResult(int processId, string title, string description,
+            float distance, float score)
+        {
+            ProcessId = processId;
+            Title = title;
+            Description = description;
+            Distance = distance;
+            Score = score;
+        }
+
+        public bool Equals(IQueryResult obj)
+        {
+            return obj != null && obj.GetType() == GetType()
+                               && Equals((ProcessResult) obj);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj != null && (obj.GetType() == GetType()
+                                   && Equals((ProcessResult) obj));
+        }
+
+        public bool Equals(ProcessResult other)
+        {
+            return other.ProcessId.Equals(ProcessId);
+        }
+
+        public override int GetHashCode()
+        {
+            return ProcessId.GetHashCode();
+        }
+    }
+}
