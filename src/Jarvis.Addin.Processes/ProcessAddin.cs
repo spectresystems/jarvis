5d0e9c76279ef1a499ad37ce76c3d41b244c1835
Unknown <jonstodle@outlook.com>
2018-01-26 00:16:30 +0100

    Add ProcessAddin

---
+++ src/Jarvis.Addin.Processes/ProcessAddin.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Jarvis.Core;
+
+namespace Jarvis.Addin.Processes
+{
+    public sealed class ProcessAddin : IAddin
+    {
+        public void Configure(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProcessProvider>().As<IQueryProvider>().SingleInstance();
+        }
+    }
+}
