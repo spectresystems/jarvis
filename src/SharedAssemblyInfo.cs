94b53e91819f80049b3ab969d59b9017377a2b3b
Patrik Svensson <patrik@patriksvensson.se>
2018-01-31 00:26:28 +0100

    (build) Updated version.

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Spectre Systems AB")]
 [assembly: AssemblyProduct("Jarvis")]
-[assembly: AssemblyVersion("0.7.0")]
-[assembly: AssemblyFileVersion("0.7.0")]
-[assembly: AssemblyInformationalVersion("0.7.0")]
+[assembly: AssemblyVersion("0.8.0.1")]
+[assembly: AssemblyFileVersion("0.8.0.1")]
+[assembly: AssemblyInformationalVersion("0.8.0-beta0001")]
 [assembly: AssemblyCopyright("Copyright (c) 2017 Spectre Systems AB")]
 
